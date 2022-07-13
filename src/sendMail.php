1a43939403b61c46ac36fd80bb23100e995d1d59
BretonLud <godneo@hotmail.fr>
2022-07-13 02:46:07 +0200

    update

--- src/sendMail.php
+++ src/sendMail.php
@@ -34,7 +34,7 @@ if ($_GET['ajax']){
 
             $message = wordwrap($message, 100, "\r\n");
 
-            $mail->isSMTP();
+            $mail->isSendmail();
 
             $mail->Host = 'localhost';
 
@@ -43,7 +43,7 @@ if ($_GET['ajax']){
             $mail->addAddress("bretonludovic40@gmail.com");
 
             $mail->addReplyTo("$email");
-
+<
             $mail->setFrom("portfolio.bretonludovic@hotmail.fr");
 
             $mail->Subject = "$objet: $name";
